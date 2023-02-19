a684077b54a9a7dc60aba5d5d0a30a693d89bec2
Salomao S <souza95salomao@gmail.com>
2023-02-19 01:04:55 +0000

    feat: linked list

---
+++ src/LinkedListo.java
@@ -0,0 +1,104 @@
+public class LinkedListo<T> {
+	private Nodo<T> entryReference;
+
+	public LinkedListo() { this.entryReference = null; }
+
+	public void add(T content) {
+		Nodo<T> newNodo = new Nodo<>(content);
+
+		if(isEmpty()) {
+			this.entryReference = newNodo;
+			return;
+		}
+
+		Nodo<T> nodoRefAux = this.entryReference;
+		for(int i = 0; i < this.size()-1; i++) {
+			nodoRefAux = nodoRefAux.getNextNodo();
+		}
+
+		nodoRefAux.setNextNodo(newNodo);
+	}
+
+	public T remove(int index)
+			throws IndexOutOfBoundsException {
+		Nodo<T> pivotNodo = getNodo(index);
+		T content = pivotNodo.getContent();
+
+		if(index == 0) {
+			this.entryReference = pivotNodo.getNextNodo();
+			return content;
+		}
+
+		Nodo<T> prevNodo = getNodo(index - 1);
+		prevNodo.setNextNodo(pivotNodo.getNextNodo());
+
+		return content;
+	}
+
+	public T get(int index) {
+		if(isEmpty()) { return null; }
+		return getNodo(index).getContent();
+	}
+
+	private Nodo<T> getNodo(int index)
+			throws IndexOutOfBoundsException {
+		validateIndex(index);
+
+		Nodo<T> nodoRefAux = this.entryReference;
+		Nodo<T> nodoResult = null;
+
+		for(int idx = 0; idx < size(); idx++) {
+			if(idx == index) {
+				nodoResult = nodoRefAux;
+				break;
+			}
+
+			nodoRefAux = nodoRefAux.getNextNodo();
+		}
+
+		return nodoResult;
+	}
+
+	private void validateIndex(int index)
+			throws IndexOutOfBoundsException {
+		if(index >= size() || index < 0) {
+			String log = "No nodo found for index " + index + "!";
+			throw new IndexOutOfBoundsException(log);
+		}
+	}
+
+	public int size() {
+		Integer _size = 0;
+		Nodo<T> nodoRefAux = this.entryReference;
+
+		while(nodoRefAux != null) {
+			_size++;
+
+			if(nodoRefAux.getNextNodo() == null) { break; }
+
+			nodoRefAux = nodoRefAux.getNextNodo();
+		}
+
+		return _size;
+	}
+
+	public boolean isEmpty() { return this.entryReference == null; }
+
+	@Override
+	public String toString() {
+		String str = "{ ";
+
+		if(isEmpty()) { return "null"; }
+
+		Nodo<T> nodoRefAux = this.entryReference;
+		
+		while(nodoRefAux != null) {
+			str += nodoRefAux + " --> ";
+			nodoRefAux = nodoRefAux.getNextNodo();
+		}
+
+		str += "null }";
+
+		return str;
+	}
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 
+
+
+
 public class Main {
 	public static void main(String[] args) {
+		LinkedListo<Object> listo = new LinkedListo<>();
+
+		for(int i=0; i<6; i++) { listo.add(i); }
+		System.out.println(listo);
+
+		// TODO: Removes list item at index 3;
+		listo.remove(3);
+		System.out.println(listo);
 	}
 }
